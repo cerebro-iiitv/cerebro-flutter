6973ee57ff0408004d6238c6c479699d29d56730
aashutoshrathi <aashutoshrathi@gmail.com>
2018-12-18 19:45:34 +0530

    Remove unused import from tests

--- test/sample_test.dart
+++ test/sample_test.dart
@@ -5,16 +5,11 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:cerebro_flutter/main.dart';
-
 void main() {
-  
   test('sample_test', () async {
     var answer = 20;
     expect(answer, 20);
   });
-
 }

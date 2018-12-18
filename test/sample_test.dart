d7ad7df5dd8108e6b01f3d73c4fabfd6df1056df
Ananthavijay <vj35.cool@gmail.com>
2018-12-18 16:56:51 +0530

    Added scripts into travis and a sample unit test

---
+++ test/sample_test.dart
@@ -0,0 +1,20 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:cerebro_flutter/main.dart';
+
+void main() {
+  
+  test('sample_test', () async {
+    var answer = 20;
+    expect(answer, 20);
+  });
+
+}

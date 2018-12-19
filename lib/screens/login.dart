42c321926ad3007ba952a90b195e44405d2b62ee
Yash Ladha <201551061@iiitvadodara.ac.in>
2018-12-19 23:23:37 +0530

    Fixes #15 : Added Shared Preferences and initial Routing (#16)

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:cerebro_flutter/models/user.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
 
 final GoogleSignIn _googleSignIn = GoogleSignIn();
 final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -10,6 +13,23 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final String _kUserPref = "UserPref";
+
+  Future<Null> getSharedPref() async {
+    final SharedPreferences _localPref = await SharedPreferences.getInstance();
+    String userProfile = _localPref.getString(_kUserPref);
+    if (userProfile != null) {
+      print('Already present user: ' + userProfile);
+      Navigator.pushNamed(context, '/events');
+    }
+  }
+
+  @override
+  initState() {
+    super.initState();
+    getSharedPref();
+  }
+
   Future<FirebaseUser> _handleSignIn() async {
     GoogleSignInAccount googleUser = await _googleSignIn.signIn();
     GoogleSignInAuthentication googleAuth = await googleUser.authentication;
@@ -18,13 +38,27 @@ class _LoginPageState extends State<LoginPage> {
       idToken: googleAuth.idToken,
     );
     print("signed in " + user.displayName);
+    assert(user != null);
+    User _loggedInUser = User.fromFirebaseUser(user);
+    print(_loggedInUser.toString());
+
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    pref.setString(_kUserPref, json.encode(_loggedInUser.toJson())).then((_) {
+      print('Shared Preference saved');
+      Navigator.pushNamed(context, '/events');
+    }).catchError((_) {
+      print('Unable to save to sharedPreference');
+    });
+
     return user;
   }
 
   Future<void> signOut() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    pref.remove(_kUserPref);
+    print('User removed');
     return _auth.signOut();
   }
-  
 
   @override
   Widget build(BuildContext context) {
@@ -34,14 +68,15 @@ class _LoginPageState extends State<LoginPage> {
           height: 100.0,
         ),
         Container(
-            padding: EdgeInsets.symmetric(
-              vertical: 120.0,
-              horizontal: 50.0,
-            ),
-            child: Image(
-              image: AssetImage('assets/images/fest-logo.png'),
-              height: 150,
-            )),
+          padding: EdgeInsets.symmetric(
+            vertical: 120.0,
+            horizontal: 50.0,
+          ),
+          child: Image(
+            image: AssetImage('assets/images/fest-logo.png'),
+            height: 150,
+          ),
+        ),
         Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -54,7 +89,9 @@ class _LoginPageState extends State<LoginPage> {
                 child: Text('Login with Google'),
                 onPressed: () => _handleSignIn(),
               ),
-              SizedBox(height: 10.0,),
+              SizedBox(
+                height: 10.0,
+              ),
               OutlineButton(
                 borderSide: BorderSide(color: Colors.deepOrange),
                 shape: RoundedRectangleBorder(
